0cbc97e90ac920b37a687532e9fee690ef563875
zhouchao <810753835@qq.com>
2018-01-29 18:10:37 +0800

    Initial commit

---
+++ uikit/src/com/netease/nim/uikit/LogConfig.java
@@ -0,0 +1,17 @@
+package com.netease.nim.uikit;
+
+/**
+ * Created by zc on 2018/1/29.
+ */
+
+public class LogConfig {
+    private static final String log = "logConfig--";
+    public static class Notify{
+        private static final String notify = log + "通知消息 ==== ";
+        public static final String notify_onclick = notify  + "点击跳转 , ";
+        public static final String notify_head = notify  + "头像 ： ";
+        public static final String notify_name = notify  + "昵称 ： ";
+        public static final String notify_im_msg = notify  + "im msg ： ";
+
+    }
+}
